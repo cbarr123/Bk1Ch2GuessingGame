7b123943b34ba72034422c4ed94cf1fdd9df35c0
Carl Barringer <cbarringer@Carls-MacBook-Pro.local>
2019-10-03 15:52:49 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumberGenerator = new Random();
+            int secretNumber = randomNumberGenerator.Next(1, 101);
+            Console.WriteLine("Please enter a difficulty level: Easy, Medium, Hard");
+            string difficultyLevel = Console.ReadLine();
+            Console.WriteLine("Please guess a number!!");
+            int numGuesses = 0;
+            if (difficultyLevel == "Easy")
+                {
+                    numGuesses = 8;
+                }
+                else if (difficultyLevel == "Medium")
+                {
+                    numGuesses = 6;
+                }
+                else
+                {
+                    numGuesses = 4;
+                }
+            for (int i = 0; i < numGuesses; i++)
+            {     
+                string guess = Console.ReadLine();
+                int intGuess = int.Parse(guess);
+                Console.Write($"Your guess ({ numGuesses - i })>");
+                if (intGuess == secretNumber) {
+
+                    Console.WriteLine("You got it right, you are amazing");
+                    break;
+                } 
+                else if (intGuess > secretNumber){
+                    Console.WriteLine("Guess is too high");
+                }
+                else //if (intGuess < secretNumber)
+                {
+                    Console.WriteLine("Guess is too low");
+                }
+            }
+        }
+    }
+}
